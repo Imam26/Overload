12416d3caba70a25ec5b89b87ac8e06099541512
Imam26 <i.m.a.m.sabitov@mail.ru>
2018-10-25 16:45:40 +0600

    first-commit

---
+++ MyClassLib/WorldOfTanks/Tank.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib.WorldOfTanks
+{
+    public class Tank
+    {
+        private static Random rand = new Random();
+        private string name;
+        private int ammunition;
+        private int armor;
+        private int mobility;
+
+        public Tank() { }
+
+        public Tank(string name)
+        {
+            Name = name;
+            Ammunition = rand.Next(0, 100);
+            Armor = rand.Next(0, 100);
+            Mobility = rand.Next(0, 100);
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Ammunition { get => ammunition; set => ammunition = value; }
+        public int Armor { get => armor; set => armor = value; }
+        public int Mobility { get => mobility; set => mobility = value; }
+
+        public static bool operator*(Tank fTank, Tank sTank)
+        {
+            if (fTank.Armor > sTank.Armor && fTank.Mobility > sTank.Mobility)
+            {
+                return true;
+            }
+            else if (fTank.Ammunition > sTank.Ammunition && fTank.Mobility > sTank.Mobility)
+            {
+                return true;
+            }
+            else if (fTank.Armor > sTank.Armor && fTank.Ammunition > sTank.Ammunition)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public string GetParametrs()
+        {
+            return string.Format($"{Ammunition}-{Armor}-{Mobility}");
+        }
+    }
+}
---
+++ Overload/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib.WorldOfTanks;
+
+namespace Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tank[] firstTanks = new Tank[5];
+            Tank[] secondTanks = new Tank[5];
+               
+            try
+            {
+                Battle(firstTanks, secondTanks);
+            }
+            catch(NullReferenceException exception)
+            {
+                Console.WriteLine("Exception: {0}", exception.Message);
+                Console.WriteLine();
+
+                InitTank(firstTanks, "T-34");
+                InitTank(secondTanks, "Panterа");
+                Battle(firstTanks, secondTanks);
+            }
+
+        }
+
+        static void InitTank(Tank[] tanks, string name)
+        {
+            for(int i = 0; i<tanks.Length; i++)
+            {
+                tanks[i] = new Tank(name);
+            }
+        }
+
+        static void Battle(Tank[] firstTanks, Tank[] secondTanks)
+        {
+            for (int i = 0; i < firstTanks.Length; i++)
+            {
+                Console.Write($"({firstTanks[i].Name}) {firstTanks[i].GetParametrs()} " +
+                    $"vs ({secondTanks[i].Name}) {secondTanks[i].GetParametrs()} result: ");
+                if (firstTanks[i] * secondTanks[i])
+                    Console.WriteLine("Winner: {0}", firstTanks[i].Name);
+                else
+                    Console.WriteLine("Winner: {0}", secondTanks[i].Name);
+            }
+        }
+    }
+}
